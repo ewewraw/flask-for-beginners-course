4b60cc776f2b88dcdc9373b9b6514ed0ce27d2bc
Gulnaz <shakirova.gul2015@yandex.ru>
2023-05-29 16:08:00 +0300

    changed main file

--- main2.py
+++ main2.py
@@ -5,9 +5,10 @@ from os import listdir
 from os.path import isfile, join
 from flask_mail import Mail, Message
 import os
+
 app = Flask(__name__)
 
-app.config['MAIL_SERVER']='smtp.mail.ru'
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.getenv("FROM_EMAIL")
 app.config['MAIL_PASSWORD'] = os.getenv("EMAIL_PASSWORD")
@@ -16,21 +17,10 @@ app.config['MAIL_USE_SSL'] = True
 
 mail = Mail(app)
 
-app_language = 'ru_RU'
-
-main_path = '.'
-locale.setlocale(locale.LC_ALL, app_language)
-languages = {}
-language_list = [f for f in listdir(os.path.join(main_path, 'language')) if
-                 isfile(join(os.path.join(main_path, 'language'), f))]
-for lang in language_list:
-    lang_code = lang.split('.')[0]
-    with open(os.path.join(main_path, 'language', lang), 'r', encoding='utf8') as file:
-        languages[lang_code] = json.loads(file.read())
-
 @app.route('/')
 def main_page():
     return render_template('portfolio.html', translations=languages.get(app_language))
+
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html', translations=languages.get(app_language))
@@ -67,12 +57,13 @@ def certificates_categories():
 def contact():
     return render_template('contact_form.html', translations=languages.get(app_language))
 
-@app.route('/send-email', methods = ['POST'])
+@app.route('/send-email', methods=['POST'])
 def send_email():
     success = True
     try:
-        msg = Message( languages.get(app_language).get('letter_hello') + request.form['lastname'] + ' ' + request.form['firstname'],
-                       sender=os.getenv("FROM_EMAIL"), recipients=[request.form['email']])
+        msg = Message(languages.get(app_language).get('letter_hello') + request.form['lastname'] + ' ' + request.form[
+            'firstname'],
+                      sender=os.getenv("FROM_EMAIL"), recipients=[request.form['email']])
         msg.body = languages.get(app_language).get('letter_subject')
         mail.send(msg)
 
@@ -92,20 +83,33 @@ def send_email():
 
     return render_template('portfolio.html', translations=languages.get(app_language), emailSentResult=result)
 
-@app.route('/set_lang', methods = ['POST'])
+@app.route('/set_lang', methods=['POST'])
 def dummy_function():
     global app_language
     lang = request.form['lang']
     path = request.form['path']
     if lang == 'en':
         app_language = 'en_EN'
-        return render_template(path+'.html', translations=languages.get(app_language))
+        return render_template(path + '.html', translations=languages.get(app_language))
     elif lang == 'ru':
         app_language = 'ru_RU'
-        return render_template(path+'.html', translations=languages.get(app_language))
+        return render_template(path + '.html', translations=languages.get(app_language))
     else:
         app_language = 'es_ES'
-        return render_template(path+'.html', translations=languages.get(app_language))
+        return render_template(path + '.html', translations=languages.get(app_language))
 
 if __name__ == '__main__':
-    app.run(debug=True) # application will start listening for web request on port 5000
+    app_language = 'ru_RU'
+
+    main_path = os.getenv("PROJECT_PATH")
+
+    locale.setlocale(locale.LC_ALL, app_language)
+    languages = {}
+    language_list = [f for f in listdir(os.path.join(main_path, 'language')) if
+                     isfile(join(os.path.join(main_path, 'language'), f))]
+    for lang in language_list:
+        lang_code = lang.split('.')[0]
+        with open(os.path.join(main_path, 'language', lang), 'r', encoding='utf8') as file:
+            languages[lang_code] = json.loads(file.read())
+
+    app.run(debug=True)
